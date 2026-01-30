76f81a1a10e91c520e66725e75a9dc70b8555da2
calvinbrown085 <calbrown@jackhenry.com>
2026-01-30 14:32:34 -0600

    Adding just blog posts

--- dist/assets/index-DM9fw7-D.js
+++ dist/assets/index-DM9fw7-D.js
@@ -133,7 +133,7 @@ logger.Info("hello, world")</code></pre>
             <svg viewBox="0 0 24 24" fill="currentColor" class="section-icon">
               <path d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-5 14H7v-2h7v2zm3-4H7v-2h10v2zm0-4H7V7h10v2z"/>
             </svg>
-            Golang Blog
+            Blog Posts
           </h2>
           
           ${this.selectedPost?this._renderFullPost():this._renderPostList()}
